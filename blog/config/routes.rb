9e9b59c1b50163c0016321c7591cbdcbf462e587
Vedd7224 <31549604+Vedd7224@users.noreply.github.com>
2018-02-15 23:10:17 -0500

    ran rails server, ran a scaffold command to generate a home index and configured some other things as well through text editors

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+    
+  resources :posts
+    #...
+    # You can have the root of your site routed with "root"
+    # just remember to delete public/index.html.
+    root :to => "home#index"
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
